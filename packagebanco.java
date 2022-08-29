3b7a556698da76e6a33616c772ec268c2496011d
Flora Fernandes Martins <83349315+florafernandes@users.noreply.github.com>
2022-08-29 17:42:39 -0300

    Package de banco
    Uso de package com classes em Java.

---
+++ packagebanco.java
@@ -0,0 +1,12 @@
+package br.com.BancoX.contas;
+
+    class Banco {
+        String nome;
+    }
+    
+package br.com.BancoY.contas;
+
+    class Cliente {
+        String nome;
+        String endereco;
+    }
