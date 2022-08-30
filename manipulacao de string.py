96f8f63270c65d1d22b87f3322b75c31adf4293b
Flora Fernandes Martins <83349315+florafernandes@users.noreply.github.com>
2022-08-30 13:20:25 -0300

    Manipulando seu nome
    Retorna seu nome manipulado de três maneiras diferentes.

---
+++ manipulacao de string.py
@@ -0,0 +1,10 @@
+seunome = str(input('Digite o seu nome: '))
+#Deixa a primeira letra sempre maiuscula
+print('Nome próprio se escreve com letra maiúscula, ajeitei pra você: ' + seunome.capitalize())
+
+#Imprime as tres primeiras letras
+print('As três primeiras letras do seu nome são: ' + seunome.capitalize()[:3])
+
+#Imprime o nome ao contrario
+print('Seu nome ao contrário é: ' +seunome.capitalize()[::-1])
+
